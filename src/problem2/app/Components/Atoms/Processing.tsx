2e71e584804fa5bc4436b5f4ead4ad5a224ae5d0
Oliver <oliver.nguyen3010@gmail.com>
2024-06-25 23:30:57 +0700

    fix: change processing style

--- src/problem2/app/Components/Atoms/Processing.tsx
+++ src/problem2/app/Components/Atoms/Processing.tsx
@@ -1,11 +1,17 @@
 const Processing = () => {
-    return <button type="button" className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-rose-600 hover:bg-rose-500 focus:border-rose-700 active:bg-rose-700 transition ease-in-out duration-150 cursor-not-allowed" disabled>
-        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
-        Processing
-    </button>
+    return <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+        <div className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-blue-400 h-12 w-12"></div>
+            <div className="flex-1 space-y-4 py-1">
+                <div className="h-4 bg-blue-400 rounded w-3/4"></div>
+                <div className="space-y-2">
+                    <div className="h-4 bg-blue-400 rounded"></div>
+                    <div className="h-4 bg-blue-400 rounded w-5/6"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 }
 
 export default Processing
